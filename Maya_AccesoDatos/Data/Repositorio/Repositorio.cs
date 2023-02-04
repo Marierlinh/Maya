e77343f0c6c0df471c231fe85d39f75d32cd0db5
Marierlin Higuerey <67464628+Marierlinh@users.noreply.github.com>
2023-02-03 20:03:44 -0500

    Interfaz de Repositorio

---
+++ Maya_AccesoDatos/Data/Repositorio/Repositorio.cs
@@ -0,0 +1,91 @@
+﻿using Maya_AccesoDatos.Data.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maya_AccesoDatos.Data.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repositorio(ApplicationDbContext db, DbSet<T> dbSet)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Agregar(T entidad)
+        {
+           dbSet.Add(entidad);
+        }
+
+        public void Grabar()
+        {
+            _db.SaveChanges();
+        }
+
+        public T Obtener(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if(filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach(var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirProp); 
+                }
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return query.FirstOrDefault();;
+        }
+
+        public IEnumerable<T> ObtenerTodos(Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if(filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach(var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirProp); 
+                }
+            }
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return query.ToList();
+        }
+
+        public void Remover(T entidad)
+        {
+            dbSet.Remove(entidad);
+        }
+    }
+}
